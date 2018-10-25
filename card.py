dcb0b58d9b0bed1708137362e21018bcfb93e79d
DrewGregory <djgregny@gmail.com>
2018-10-24 21:23:44 -0700

    better doc

--- card.py
+++ card.py
@@ -9,6 +9,12 @@ class Card:
     def __init__(self, index):
         """ 
         Indices: 0 - 12 -> Ace thru King of Spades
+        0 -> 2 of Spades
+        1 -> 3 of Spades
+        ...
+        10 -> Queen of Spades
+        11 -> King of Spades
+        12 -> Ace of Spades
         13 - 25 -> Clubs
         26 - 38 -> Hearts
         39 - 51 -> Diamonds
