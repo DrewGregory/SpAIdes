1219fb49c07ef8a0a70a75cea063589072b10985
DrewGregory <djgregny@gmail.com>
2018-11-20 19:14:29 -0500

    fix conflicts

--- agents.py
+++ agents.py
@@ -52,12 +52,7 @@ class ModelPlayer(Baseline):
         #print("bestChoice: " + str(bestQ[1]))
         assert not bestQ[0] == None
         self.bid = bestQ[1]
-<<<<<<< HEAD
         print("BID " + str(self.bid))
-=======
-        #print("BID " + str(self.bid))
-        
->>>>>>> 99d292d64e5476eb61c2a114439237e2a40fd7c5
         return self.bid
     
 
@@ -187,13 +182,8 @@ class ModelTest(ModelPlayer):
     def __init__(self, hand, name=""):
 
         
-<<<<<<< HEAD
         learning_rate = 5e-2 # usually a reasonable val
         LEN_FEATURE_VECTOR =      52    +          52      +     4    +  52   +  4  +  4  + 52
-=======
-        learning_rate = 1e- # usually a reasonable val
-        LEN_FEATURE_VECTOR = 57 # 52    +          52      +     14*4     +  52   +  4  +  4  + 52
->>>>>>> 99d292d64e5476eb61c2a114439237e2a40fd7c5
         #                    playerCards    claimedCards    playerBids   pile    tricks       
         
         
